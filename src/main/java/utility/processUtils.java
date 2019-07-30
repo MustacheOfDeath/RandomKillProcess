257239fb4b3531c66958579211219dad7f057ae5
Lorenzo F <lorenzo.filattiera@esternibisp.com>
2019-07-30 10:07:43 +0200

    SomeChanges

--- src/main/java/utility/processUtils.java
+++ src/main/java/utility/processUtils.java
@@ -2,7 +2,6 @@ package utility;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -44,41 +43,41 @@ public class processUtils {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println(processList.size()+" killable app\n\n");
+        System.out.println(processList.size() + " killable app\n\n");
         return processList;
     }
 
     public static void execKill(List<String> killApp) throws InterruptedException {
-        for(int i=0;i<killApp.size();i++) {
+        for (int i = 0; i < killApp.size(); i++) {
             try {
                 System.out.print("Killing " + killApp.get(i));
                 String killSkype = "Skype.exe";
                 Runtime.getRuntime().exec("TASKKILL /F /IM " + killSkype);
                 /*
-                *
-                * Runtime.getRuntime().exec("TASKKILL /F /IM " +killApp.get(i);
-                *
-                */
+                 *
+                 * Runtime.getRuntime().exec("TASKKILL /F /IM " +killApp.get(i);
+                 *
+                 */
                 TimeUnit.MILLISECONDS.sleep(10);
                 System.out.println("-----> KILLED");
             } catch (IOException ioe) {
                 ioe.printStackTrace();
             }
         }
-        System.out.println("\n\nKilled "+killApp.size()+" app");
+        System.out.println("\n\nKilled " + killApp.size() + " app");
     }
 
     public static List<String> createRandomList(List<String> listToRandomize) {
         List<String> randomList = new ArrayList<>();
         Random randomGenerator = new Random();
         List<Integer> previousNum = new ArrayList<>();
-        do{
-            int randomNum = randomGenerator.nextInt( listToRandomize.size());
-            if (!previousNum.contains(randomNum)){
+        do {
+            int randomNum = randomGenerator.nextInt(listToRandomize.size());
+            if (!previousNum.contains(randomNum)) {
                 previousNum.add(randomNum);
                 randomList.add(listToRandomize.get(randomNum));
             }
-        }while (randomList.size()<(listToRandomize.size()/2));
+        } while (randomList.size() < (listToRandomize.size() / 2));
         return randomList;
     }
 }
